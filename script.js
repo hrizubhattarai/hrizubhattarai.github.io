8264f3859e35427a8c42e3bb05fec39ca9ed595e
hrizu-bhattarai <112821997+hrizubhattarai@users.noreply.github.com>
2022-12-14 16:59:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,85 +1,93 @@
+// Creating "close" button
 
-const todoList = [];
+var list = document.getElementsByTagName("list");
 
-const todoListElement = document.querySelector("#myUL");
+var i;
 
-document.querySelector("#add_button").addEventListener("click", addTodo);
-document.querySelector("#myInput").addEventListener("keydown", function(e) {
-  if (e.keyCode == 13) {
-    addTodo()
+for (i = 0; i < list.length; i++) {
+
+  var s = document.createElement("SPAN");
+
+  var txt = document.createTextNode("\u00D7");
+
+  s.className = "close";
+
+  s.appendChild(txt);
+
+  list[i].appendChild(s);
+
+}
+
+
+
+// Click on close button to hide list element
+
+var c = document.getElementsByClassName("close");
+
+var i;
+
+for (i = 0; i < c.length; i++) {
+
+  c[i].onclick = function() {
+
+    var d = this.parentElement;
+
+    d.style.display = "none";
+
   }
-});
 
-//getting input and arranging it into array
-function addTodo() {
-  const todoText = document.querySelector("#myInput").value;
+}
 
-  if (todoText == "") {
-    alert("You did not enter any item");
+
+
+// Creating new list item when clicking on "Add" button
+
+function newElement() {
+
+  var a = document.createElement("li");
+
+  var Value = document.getElementById("myInput").value;
+
+  var t = document.createTextNode(Value);
+
+  a.appendChild(t);
+
+  if (Value === '') {
+
+    alert("The task cannot be empty");
+
   } else {
-    const todoObject = {
-      id: todoList.length,
-      todoText: todoText,
-      isDone: false,
-    };
 
-    //unshift adds new element to the beginning of the array, newly added item is shown at the top
-    todoList.unshift(todoObject);
-    displayTodos();
+    document.getElementById("myUL").appendChild(a);
+
   }
-}
 
-//the isdone value is changed to true when the element is clicked, if you click again, it changes it False
-function doneTodo(todoId) {
-  const selectedTodoIndex = todoList.findIndex((item) => item.id == todoId);
+  document.getElementById("myInput").value = "";
 
-  todoList[selectedTodoIndex].isDone
-    ? (todoList[selectedTodoIndex].isDone = false)
-    : (todoList[selectedTodoIndex].isDone = true);
-  displayTodos();
-}
+  var s = document.createElement("SPAN");
 
-//adding function to delete items from the list
-function deleteItem(x) {
-  todoList.splice(
-    todoList.findIndex((item) => item.id == x),
-    1
-  );
-  displayTodos();
-}
+  var txt = document.createTextNode("\u00D7");
 
-//adding function to display the items added onto the screen
-function displayTodos() {
-  todoListElement.innerHTML = "";
-  document.querySelector("#myInput").value = "";
+  s.className = "close";
 
-  todoList.forEach((item) => {
-    const listElement = document.createElement("li");
-    const delBtn = document.createElement("i");
+  s.appendChild(txt);
 
-    listElement.innerHTML = item.todoText;
-    listElement.setAttribute("data", item.id);
+  a.appendChild(s);
 
-    delBtn.setAttribute("data", item.id);
-    delBtn.classList.add("far");
-    delBtn.classList.add("fa-trash-alt");
-    delBtn.setAttribute("data", item.id);
 
-    if (item.isDone) {
-      listElement.classList.add("checked");
+
+  for (i = 0; i < c.length; i++) {
+
+    c[i].onclick = function() {
+
+      var d = this.parentElement;
+
+      d.style.display = "none";
+
     }
 
-    listElement.addEventListener("click", function (e) {
-      const selectedId = e.target.getAttribute("data");
-      doneTodo(selectedId);
-    });
+  }
 
-    delBtn.addEventListener("click", function (e) {
-      const delId = e.target.getAttribute("data");
-      deleteItem(delId);
-    });
-
-    todoListElement.appendChild(listElement);
-    listElement.appendChild(delBtn);
-  });
 }
+
+
